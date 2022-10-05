4c7ac37c85d5686fea911a654fc5025d500b9d50
พงศ์วิสิษฎ์ <101461240+Ashuu0@users.noreply.github.com>
2022-10-05 19:15:36 +0700

    api 1.2

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -9,8 +9,10 @@ function Login() {
   const [state,setState] = useState({
     ID:"",
     PASSWORD:"",
+    CONFIRM_PASSWORD:"",
+    NAME:""
   })
-  const {ID,PASSWORD} = state
+  const {ID,PASSWORD,CONFIRM_PASSWORD,NAME} = state
   //กำหนดค่าstate
   const inputValue=name=>event=>{
     //console.log(name,"=",event.target.value)
@@ -18,10 +20,10 @@ function Login() {
   }
   const submitForm=(e)=>{
     e.preventDefault();
-    console.table({ID,PASSWORD})
+    console.table({ID,PASSWORD,CONFIRM_PASSWORD,NAME})
     console.log("API URL = ",process.env.REACT_APP_API)
     axios
-    .post(String(process.env.REACT_APP_API)+'/login',{ID,PASSWORD})
+    .post(String(process.env.REACT_APP_API)+'/login',{ID,PASSWORD,CONFIRM_PASSWORD,NAME})
     .then(response=>{alert("complete")})
     .catch(err=>{alert(err.response.data.error)})
   }
@@ -39,6 +41,14 @@ function Login() {
           <div className='inputPASSWORD'>
             <label>Enter your PASSWORD</label>
             <input type="text" value={PASSWORD} onChange={inputValue("PASSWORD")}></input>
+          </div>
+          <div className='inputCONFIRM_PASSWORD'>
+            <label>CONFIRM your PASSWORD</label>
+            <input type="text" value={CONFIRM_PASSWORD} onChange={inputValue("CONFIRM_PASSWORD")}></input>
+          </div>
+          <div className='inputNAME'>
+            <label>Enter your NAME</label>
+            <input type="text" value={NAME} onChange={inputValue("NAME")}></input>
           </div>
         </div>
         <button onClick={submitForm}>Submit</button>
