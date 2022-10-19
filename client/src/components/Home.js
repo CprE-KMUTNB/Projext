6b4b5b699b0531dc0af7b094741502e8cc3f52f1
Peroroncino <s6401012620129@email.kmutnb.ac.th>
2022-10-19 13:57:25 +0700

    Home.js
    button change

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,17 +7,17 @@ import MovingText from 'react-moving-text'
 
 
 const Button = styled.button`
-  background-color : #25316D;
-  color : white;
-  padding : 10px 25px;
+  background-color : white;
+  color : black;
+  padding : 5px 20px;
   border-radius : 8px;
   text-transform : uppercase;
-  font-size: 2.5em;
+  font-size: 1.5em;
   font-weight: bold;
   outline : 0;
   cursor : pointer;
-  box-shadow : 0px 2px 2px lightgray;
-  transition :ease background-color 350ms;
+  transition: ease background-color 850ms;
+  font-family:'Poppins';
   &:hover {
       background-color : #F5EDDC;
       color : black;
@@ -44,7 +44,7 @@ function Home() {
             <MyAnimatedTypo /> 
           </div>
         </div>
-        <Link to = "/register"><Button>try it for free</Button></Link>
+        <Link to = "/register"><Button>register and try for free</Button></Link>
       </div>
     </>
   )
